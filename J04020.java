d8155d501432c0d95611c4e4a40c9feb45a4e62e
nevir2002 <nevir2002@gmail.com>
2022-09-20 12:49:25 +0700

    Week 06

--- J04020.java
+++ J04020.java
@@ -4,30 +4,37 @@ import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
-class Pair<T,S>{
-    
+class Pair<T, S> {
+
     T n1;
     S n2;
     String s;
-    Pair(T t1, S t2){
-        
+
+    Pair(T t1, S t2) {
+
         n1 = t1;
         n2 = t2;
         s = String.valueOf(n1) + " " + String.valueOf(n2);
-        
+
     }
-    
-    public String toString(){
-        
+
+    public String toString() {
+
         return s;
-        
+
     }
-    
-    public boolean primeCheck(int n){
-        
-        if (n < 2) return false;
-        if (n == 2) return true;
-        if(n%2 == 0) return false;
+
+    public boolean primeCheck(int n) {
+
+        if (n < 2) {
+            return false;
+        }
+        if (n == 2) {
+            return true;
+        }
+        if (n % 2 == 0) {
+            return false;
+        }
         for (int i = 3; i * i <= n; i += 2) {
 
             if (n % i == 0) {
@@ -36,67 +43,76 @@ class Pair<T,S>{
 
         }
         return true;
-        
+
     }
-    
+
     public boolean isPrime() {
 
         return primeCheck(Integer.parseInt(String.valueOf(n1))) && primeCheck(Integer.parseInt(String.valueOf(n2)));
 
     }
-    
+
 }
 
 public class J04020 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t-->0){
+        while (t-- > 0) {
             int n = sc.nextInt();
             boolean check = false;
-            for(int i = 2; i <= 2*Math.sqrt(n); i++){
-                Pair<Integer, Integer> p = new Pair<>(i, n-i);
-                if(p.isPrime()){
+            for (int i = 2; i <= 2 * Math.sqrt(n); i++) {
+                Pair<Integer, Integer> p = new Pair<>(i, n - i);
+                if (p.isPrime()) {
                     System.out.println(p);
                     check = true;
-                    break; 
+                    break;
                 }
             }
-            if(!check) System.out.println(-1);
+            if (!check) {
+                System.out.println(-1);
+            }
         }
     }
+
     public static void main9270798(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t-->0){
+        while (t-- > 0) {
             int n = sc.nextInt();
             boolean check = false;
-            for(int i = 2; i <= 2*Math.sqrt(n); i++){
-                Pair<Integer, Integer> p = new Pair<>(i, n-i);
-                if(p.isPrime()){
+            for (int i = 2; i <= 2 * Math.sqrt(n); i++) {
+                Pair<Integer, Integer> p = new Pair<>(i, n - i);
+                if (p.isPrime()) {
                     System.out.println(p);
                     check = true;
-                    break; 
+                    break;
                 }
             }
-            if(!check) System.out.println(-1);
+            if (!check) {
+                System.out.println(-1);
+            }
         }
     }
+
     public static void main49771(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t-->0){
+        while (t-- > 0) {
             int n = sc.nextInt();
             boolean check = false;
-            for(int i = 2; i <= 2*Math.sqrt(n); i++){
-                Pair<Integer, Integer> p = new Pair<>(i, n-i);
-                if(p.isPrime()){
+            for (int i = 2; i <= 2 * Math.sqrt(n); i++) {
+                Pair<Integer, Integer> p = new Pair<>(i, n - i);
+                if (p.isPrime()) {
                     System.out.println(p);
                     check = true;
-                    break; 
+                    break;
                 }
             }
-            if(!check) System.out.println(-1);
+            if (!check) {
+                System.out.println(-1);
+            }
         }
     }
 
